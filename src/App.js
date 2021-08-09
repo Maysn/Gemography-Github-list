0025868a9e086fd9967269e433d6c9cdb00a96a0
Maysn <mmazen1321@gmail.com>
2021-08-09 21:13:53 +0200

    make scroll bottom detection function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-// import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import ReposList from './components/reposList';
 
 function App() {
-  
+  const [reachedBot, setReachedBot] =useState(false);
+  const handleScroll = (e) => {
+    const bottom = e.target.scrollHeight - e.target.scrollTop === e.target.clientHeight;
+    if (bottom) { 
+        console.log("bottom")
+        setReachedBot(true);
+    }
+ }  
   return (
-    <div className="App">
-      <ReposList/>
+    <div className="App" onScroll={handleScroll}>
+      <ReposList reachedBot={reachedBot} setReachedBot={setReachedBot}/>
     </div>
   );
 }
--- src/components/reposList.js
+++ src/components/reposList.js
@@ -1,16 +1,16 @@
 import React, { useEffect, useState } from 'react';
 
-function ReposList(props) {
+function ReposList({reachedBot, setReachedBot}) {
     const [RepositoriesList, setRepositoriesList] = useState();
     useEffect(() => {
         fetch('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
         .then(response => response.json())
-        .then(list => setRepositoriesList(list))
+        .then(list => setRepositoriesList(list.items))
     },[])
     console.log(RepositoriesList)
 
-    const repositoryDetails = RepositoriesList?.items.map((repo) => {
-        return <div key={repo.id} className="repository">
+    const repositoryDetails = RepositoriesList?.map((repo) => {
+        return <div key={repo.full_name} className="repository">
             <div className="repository__avatar">
                 <img src={repo.owner.avatar_url} alt="Repoistory Avatar"/>
             </div>

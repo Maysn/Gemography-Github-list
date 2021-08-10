cfde83cc45cceb3fc9d769b76f0a6a34c563e35f
Maysn <mmazen1321@gmail.com>
2021-08-10 23:13:53 +0200

    potentially fix fetching more lists on phone

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import { useState } from 'react';
-import './App.css';
-import ReposList from './containers/reposList';
+import { useState } from "react";
+import "./App.scss";
+import ReposList from "./containers/reposList";
 
 function App() {
-  const [reachedBot, setReachedBot] =useState(false);
+  const [reachedBot, setReachedBot] = useState(false);
   const handleScroll = (e) => {
-    const bottom = Math.ceil(e.target.scrollHeight - e.target.scrollTop) === e.target.clientHeight;
-    if (bottom) { 
-        console.log("bottom")
-        console.log(e.target.clientHeight)
-        console.log(e.target.scrollHeight - e.target.scrollTop)
-        setReachedBot(true);
+    // const bottom = Math.ceil(e.target.scrollHeight - e.target.scrollTop) === e.target.clientHeight;
+    const bottom =
+      e.target.clientHeight + e.target.scrollTop + 50 >= e.target.scrollHeight;
+    if (bottom) {
+      console.log("bottom");
+      console.log(e.target.clientHeight);
+      console.log(e.target.scrollHeight - e.target.scrollTop);
+      setReachedBot(true);
     }
- }  
+  };
   return (
     <div className="App" onScroll={handleScroll}>
-      <ReposList reachedBot={reachedBot} setReachedBot={setReachedBot}/>
+      <ReposList reachedBot={reachedBot} setReachedBot={setReachedBot} />
     </div>
   );
 }
--- src/containers/reposList.js
+++ src/containers/reposList.js
@@ -1,53 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-function ReposList({reachedBot, setReachedBot}) {
-    const [RepositoriesList, setRepositoriesList] = useState();
-    const [count, setCount] = useState(2);
+function ReposList({ reachedBot, setReachedBot }) {
+  const [RepositoriesList, setRepositoriesList] = useState();
+  const [count, setCount] = useState(2);
 
-    useEffect(() => {
-        fetch('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
-        .then(response => response.json())
-        .then(list => setRepositoriesList(list.items))
-    },[])
-    console.log(RepositoriesList)
-    
-useEffect(()=> {
-    if(reachedBot){
-        fetch(`https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${count}`)
-        .then(response => response.json())
-        .then(data => setRepositoriesList(RepositoriesList => RepositoriesList.concat(data.items)))
-        setCount(count => count + 1)
+  //   fetching initial list
+  useEffect(() => {
+    fetch(
+      "https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc"
+    )
+      .then((response) => response.json())
+      .then((list) => setRepositoriesList(list.items));
+  }, []);
+  console.log(RepositoriesList);
+
+  //   fetching following lists on reachsing bottom
+  useEffect(() => {
+    if (reachedBot) {
+      fetch(
+        `https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=${count}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setRepositoriesList((RepositoriesList) => [
+            ...RepositoriesList,
+            ...data.items,
+          ]);
+          setReachedBot(false);
+          setCount((count) => count + 1);
+        });
     }
-},[reachedBot, count])
+  }, [reachedBot, count]);
+  console.log(count);
 
-console.log(count)
-
-useEffect(()=>{
-    setReachedBot(false);
-
-})
-
-    const repositoryDetails = RepositoriesList?.map((repo) => {
-        return <div key={repo.full_name} className="repository">
-            <div className="repository__avatar">
-                <img src={repo.owner.avatar_url} alt="Repoistory Avatar"/>
-            </div>
-            <div className="repository__details">
-                <h3 className="repository__name">{repo.name}</h3>
-                <div className="repository__description">{repo.description}</div>
-                <div className="repository__stats">
-                    <span className="repository__stars">{repo.stargazers_count}</span>
-                    <span className="repository__issues">{repo.open_issues_count}</span>
-                    <span className="repository__date">{repo.pushed_at}</span>
-                </div>
-            </div>
-        </div>
-    })
+  const repositoryDetails = RepositoriesList?.map((repo) => {
     return (
-        <div>
-            {repositoryDetails}
+      <div key={repo.full_name} className="repository">
+        <div className="repository__avatar">
+          <img src={repo.owner.avatar_url} alt="Repoistory Avatar" />
         </div>
+        <div className="repository__details">
+          <h3 className="repository__name">{repo.name}</h3>
+          <div className="repository__description">{repo.description}</div>
+          <div className="repository__stats">
+            <span className="repository__stars">{repo.stargazers_count}</span>
+            <span className="repository__issues">{repo.open_issues_count}</span>
+            <span className="repository__date">{repo.pushed_at}</span>
+          </div>
+        </div>
+      </div>
     );
+  });
+  return <div>{repositoryDetails}</div>;
 }
 
 export default ReposList;
